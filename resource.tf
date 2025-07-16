4e20afd2189683339d050acf21362389b5cc4a7b
ravinderrao6789@gmail.com <ravinderrao6789@gmail.com>
2025-07-16 20:52:02 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -3,66 +3,42 @@ resource "azurerm_resource_group" "terra2" {
   location = "East US"
 }
 
-resource "azurerm_resource_group" "storeterra" {
-    name     = "example-resources2"
-    location = "East US"
+resource "azurerm_virtual_network" "terra_vnet" {
+  name                = "terra-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.terra2.location
+  resource_group_name = azurerm_resource_group.terra2.name
 }
 
-resource "azurerm_storage_account" "saterra" {
-    name                     = "niha2582"
-    resource_group_name      = azurerm_resource_group.storeterra2.name
-    location                 = azurerm_resource_group.storeterra2.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-}
- 
-resource "azurerm_storage_container" "sacontainer" {
-    name                  = "terra-container"
-    storage_account_name  = azurerm_storage_account.saterra.name
-    container_access_type = "private"
-}
-
-resource "azurerm_virtual_network" "terra2_vnet" {
-    name                = "terra2-vnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = azurerm_resource_group.terra2.location
-    resource_group_name = azurerm_resource_group.terra2.name
-}
- 
-resource "azurerm_subnet" "subnet1" {
-    name                 = "subnet1"
-    resource_group_name  = azurerm_resource_group.terra2.name
-    virtual_network_name = azurerm_virtual_network.terra_vnet.name
-    address_prefixes     = ["10.0.1.0/24"]
-}
- 
-resource "azurerm_subnet" "subnet2" {
-    name                 = "subnet2"
-    resource_group_name  = azurerm_resource_group.terra2.name
-    virtual_network_name = azurerm_virtual_network.terra2_vnet.name
-    address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_subnet" "terra_subnet" {
+  name                 = "terra-subnet"
+  resource_group_name  = azurerm_resource_group.terra2.name
+  virtual_network_name = azurerm_virtual_network.terra_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "terra_nic" {
   name                = "terra-nic"
-  location            = azurerm_resource_group.terra2_vnet.location
-  resource_group_name = azurerm_resource_group.terra2_vnet.name
+  location            = azurerm_resource_group.terra2.location
+  resource_group_name = azurerm_resource_group.terra2.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet1.id
+    subnet_id                     = azurerm_subnet.terra_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_windows_virtual_machine" "terra_vm" {
-  name                  = "terra-vm"
-  location              = azurerm_resource_group.terra.location
-  resource_group_name   = azurerm_resource_group.terra.name
-  size                  = "Standard_B2s"
-  admin_username        = "azureuser"
-  admin_password        = "P@ssword1234!"  # Use a secure password in production
-  network_interface_ids = [azurerm_network_interface.terra2_nic.id]
+  name                = "terra-winvm"
+  resource_group_name = azurerm_resource_group.terra2.name
+  location            = azurerm_resource_group.terra2.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  admin_password      = "AdminPassword123!" # Change this to a secure password
+  network_interface_ids = [
+    azurerm_network_interface.terra_nic.id,
+  ]
 
   os_disk {
     caching              = "ReadWrite"
@@ -72,7 +48,7 @@ resource "azurerm_windows_virtual_machine" "terra_vm" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2022-Datacenter"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
 }
